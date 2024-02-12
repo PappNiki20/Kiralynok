01654373839b2822167841c06c3e7f6ddc345bc6
PappNiki20 <papp.nikoletta001@gmail.com>
2024-02-12 13:44:49 +0100

    3.feladat konstruktor elkeszitese

--- kiralynok/src/model/Tabla.java
+++ kiralynok/src/model/Tabla.java
@@ -1,6 +1,17 @@
 
 package model;
 public class Tabla {
-      private char[][] T;
+      private char[][] T = new char[8][8];;
        private char UresCella;
+       
+//3.feladat konstruktor elkeszitese
+    public Tabla(char c) {
+        UresCella = c;
+
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+                T[i][j] = UresCella;
+            }
+        }
+    }
 }
