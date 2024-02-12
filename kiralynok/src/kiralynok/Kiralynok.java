d701a4913ea9c1f1259de56e50e366adad14c7bd
PappNiki20 <papp.nikoletta001@gmail.com>
2024-02-12 15:17:27 +0100

    konzolos resz megvan

--- kiralynok/src/kiralynok/Kiralynok.java
+++ kiralynok/src/kiralynok/Kiralynok.java
@@ -1,30 +1,10 @@
 package kiralynok;
 
-import model.Tabla;
+import view.Konzolos;
 
 public class Kiralynok {
 
     public static void main(String[] args) {
-
-        Tabla t = new Tabla('*');
-        char[][] matrix = t.getT();
-         String fajlbaIrok="";
-        kiir(t.Megjelenit(matrix));
-        fajlbaIrok+=""+t.Megjelenit(matrix);
-        t.Elhelyez(8, matrix);
-        kiir(t.Megjelenit(matrix));
-        fajlbaIrok+=""+t.Megjelenit(matrix);
-        
-        kiir("Üres oszlopok: " + t.UresOszlop(3));
-        kiir("Üres Sorok: " + t.UresSor(1));
-        kiir("Üres Oszlopok száma" + t.UresOszlopokSzama());
-        kiir("Üres sorok száma" + t.UresSorokSzama());
-       
-        t.FajlbaIr(fajlbaIrok);
+        new Konzolos().run();
     }
-
-    private static void kiir(String Megjelenit) {
-        System.out.println(Megjelenit);
-    }
-
 }
---
+++ kiralynok/src/view/Konzolos.java
@@ -0,0 +1,29 @@
+
+package view;
+
+import model.Tabla;
+
+public class Konzolos {
+    public void run(){
+         Tabla t = new Tabla('*');
+        char[][] matrix = t.getT();
+         String fajlbaIrok="";
+        kiir(t.Megjelenit(matrix));
+        fajlbaIrok+=""+t.Megjelenit(matrix);
+        t.Elhelyez(8, matrix);
+        kiir(t.Megjelenit(matrix));
+        fajlbaIrok+=""+t.Megjelenit(matrix);
+        
+        kiir("Üres oszlopok: " + t.UresOszlop(3));
+        kiir("Üres Sorok: " + t.UresSor(1));
+        kiir("Üres Oszlopok száma" + t.UresOszlopokSzama());
+        kiir("Üres sorok száma" + t.UresSorokSzama());
+       
+        t.FajlbaIr(fajlbaIrok);
+    }
+
+    private static void kiir(String Megjelenit) {
+        System.out.println(Megjelenit);
+    }
+    }
+
